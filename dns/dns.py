5e477054dac5f9a66e89f89af577f22a4c78338e
aleneus <aleneus@gmail.com>
2021-10-29 20:37:16 +0300

    feat: dns db

--- dns/dns.py
+++ dns/dns.py
@@ -2,20 +2,32 @@
 
 
 class NetworkInterface:
+    """Network interface."""
+
     def __init__(self):
         self.net = None
         self.addr = None
+        self.dns = None
 
     def setup(self, net, addr):
         """Set net and address to interface."""
         self.net = net
         self.addr = addr
 
+    def set_dns(self, addr):
+        # TODO docs:
+        self.dns = addr
+
+    def resolve(self, name):
+        # TODO docs:
+        return self.net.resolve(self.dns, name)
+
 
 class Comp:
     """Computer."""
     def __init__(self):
         self.__iface = NetworkInterface()
+        self.__local_db = None
 
     def set_net(self, net, addr):
         """Connect computer to net."""
@@ -29,6 +41,26 @@ class Comp:
 
         return self.__iface.net.ping(
             self.__iface.addr, addr)
+
+    def resolve(self, name):
+        # TODO docs:
+        if self.__local_db:
+            addr = self.__local_db.resolve(name)
+            if addr:
+                return addr
+
+        if not self.__iface.net:  # TODO: FIX
+            return None
+
+        return self.__iface.resolve(name)
+
+    def set_dns_db(self, db):
+        # TODO docs:
+        self.__local_db = db
+
+    def set_dns_server(self, addr):
+        # TODO ref: access to iface
+        self.__iface.set_dns(addr)
 
 
 class Net:
@@ -48,3 +80,53 @@ class Net:
             return f"ping from {src} to {dst}"
 
         return "Unknown host"
+
+    def resolve(self, dns_addr, name):
+        # TODO test: no host
+        try:
+            return self.__hosts[dns_addr].resolve(name)
+        except KeyError:
+            return None
+
+
+class Record:
+    """DNS record."""
+    def __init__(self, name, addr):
+        self.__name = name
+        self.__addr = addr
+
+    def get_name(self):
+        return self.__name
+
+    def get_addr(self):
+        return self.__addr
+
+    # TODO feat: ? + update_record()
+
+
+class DnsDb:
+    """DNS database."""
+    def __init__(self):
+        self.__records = {}
+        self.__addrs = {}
+
+    def num_records(self):
+        """Return number of records."""
+        return len(self.__records)
+
+    def add_record(self, record):
+        """Add record."""
+        self.__check_record(record)
+        self.__records[record.get_name()] = record
+
+    def resolve(self, name):
+        """Return IP address by name."""
+        try:
+            return self.__records[name].get_addr()
+        except KeyError:
+            return None
+
+    def __check_record(self, record):
+        if record.get_addr() in self.__addrs:
+            raise ValueError("Duplicated address")
+        self.__addrs[record.get_addr()] = True
--- dns/test_dns.py
+++ dns/test_dns.py
@@ -1,6 +1,8 @@
 import unittest
 from dns import Comp
 from dns import Net
+from dns import DnsDb, Record
+# TODO: extract modules
 
 
 class TestComp(unittest.TestCase):
@@ -29,3 +31,88 @@ class TestNet(unittest.TestCase):
         ans = comp2.ping("1.2.3.4")
         self.assertEqual(
             ans, "ping from 2.3.4.5 to 1.2.3.4")
+
+        ans = comp1.ping("3.4.5.6")
+        self.assertEqual(ans, "Unknown host")
+
+
+class TestDnsDb(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(DnsDb().num_records(), 0)
+
+    def test_add_record(self):
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "1.2.3.4"))
+        self.assertEqual(db.num_records(), 1)
+
+    def test_get_addr_known(self):
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "1.2.3.4"))
+        addr = db.resolve("narfu.ru")
+        self.assertEqual(addr, "1.2.3.4")
+
+    def test_get_addr_known_2(self):
+        # TODO test, ref: merge with prev
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "2.3.4.5"))
+        addr = db.resolve("narfu.ru")
+        self.assertEqual(addr, "2.3.4.5")
+
+    def test_get_addr_unknown(self):
+        # TODO: implement
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "2.3.4.5"))
+        db.add_record(Record("29.ru", "3.4.5.6"))
+        addr = db.resolve("narfu.com")
+        self.assertIsNone(addr)
+
+    def test_same_addresses_differ_names(self):
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "2.3.4.5"))
+
+        raised = False
+        try:
+            db.add_record(Record("29.ru", "2.3.4.5"))
+        except ValueError:
+            raised = True
+
+        self.assertTrue(raised)
+
+
+class TestDns(unittest.TestCase):
+    def test_no_addr(self):
+        comp = Comp()
+        ans = comp.resolve("narfu.ru")
+        self.assertEqual(ans, None)  # TODO ref: use standard error
+
+    def test_answer_in_local_db(self):
+        comp = Comp()
+        db = DnsDb()
+        db.add_record(Record("narfu.ru", "1.2.3.4"))
+        comp.set_dns_db(db)
+        ans = comp.resolve("narfu.ru")
+        self.assertEqual(ans, "1.2.3.4")
+
+    def test_answer_from_dns_server(self):
+        comp = Comp()
+        local_db = DnsDb()
+        local_db.add_record(Record("narfu.ru", "1.2.3.4"))
+        comp.set_dns_db(local_db)
+        # TODO ref: comp: set_dns_server: replace to network interface
+        comp.set_dns_server("10.20.30.40")
+        # TODO test: no such host (dns server)
+
+        server = Comp()
+        server_db = DnsDb()
+        server_db.add_record(Record("ya.ru", "2.3.4.5"))
+        server.set_dns_db(server_db)
+
+        net = Net()
+        net.add_host(comp, "11.12.13.14")
+        net.add_host(server, "10.20.30.40")
+
+        ans = comp.resolve("maps.ya.ru")
+        self.assertEqual(ans, "2.3.4.5")
+
+    # TODO test: comp: local db: resolve unknown name
+    # TODO feat: comp: ping name
